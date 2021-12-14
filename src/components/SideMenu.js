3ec526f62d584cf216f4ff257837720ea8988b79
OnlyForward0613 <harryborison613@gmail.com>
2021-12-15 01:29:00 +0900

    connect wallet

--- src/components/SideMenu.js
+++ src/components/SideMenu.js
@@ -1,45 +1,50 @@
-import { Button } from "@mui/material";
+import { ConnectButton, MenuButton } from './styleHook';
 
-export default function SideMenu() {
-    return (
-        <div className="side-menu">
-            <div className="side-logo">
-                <h2>Game Of Risk</h2>
-            </div>
-            <div className="owner-info">
-                <Button variant="contained" fullWidth>
-                    Connect
-                </Button>
-            </div>
-            <div className="side-content">
-                <ul>
-                    <li>
-                        <Button fullWidth>
-                            Mint
-                        </Button>
-                    </li>
-                    <li>
-                        <Button fullWidth>
-                            Farm
-                        </Button>
-                    </li>
-                    <li>
-                        <Button fullWidth>
-                            Train
-                        </Button>
-                    </li>
-                    <li>
-                        <Button fullWidth>
-                            unstake&nbsp;&amp;&nbsp;Claim
-                        </Button>
-                    </li>
-                    <li>
-                        <Button fullWidth>
-                            Pillage
-                        </Button>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    )
+export default function SideMenu({
+  connectWallet,
+  connected,
+  address,
+  ...props
+}) {
+  return (
+    <div className="side-menu">
+      <div className="side-logo">
+        <h2>GAME OF RISK</h2>
+      </div>
+      <div className="owner-info">
+        <ConnectButton variant="contained" fullWidth onClick={connectWallet} disabled={connected}>
+          {!connected ? "Connect" : "0x" + address.slice(2, 5) + "..." + address.slice(38, 42)}
+        </ConnectButton>
+      </div>
+      <div className="side-content">
+        <ul>
+          <li>
+            <MenuButton fullWidth style={{ borderTop: '1px solid #ccc' }}>
+              Mint
+            </MenuButton>
+          </li>
+          <li>
+            <MenuButton fullWidth>
+              Farm
+            </MenuButton>
+          </li>
+          <li>
+            <MenuButton fullWidth>
+              Train
+            </MenuButton>
+          </li>
+          <li>
+            <MenuButton fullWidth>
+              unstake&nbsp;&amp;&nbsp;Claim
+            </MenuButton>
+          </li>
+          <li>
+            <MenuButton fullWidth>
+              Pillage
+            </MenuButton>
+          </li>
+        </ul>
+      </div>
+    </div>
+  )
 }
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,12 +1,67 @@
+import { useEffect, useState } from 'react'
 import SideMenu from '../components/SideMenu'
 import '../styles/globals.css'
 import '../styles/style.scss'
+import Web3 from "web3"
+import Web3Modal from "web3modal"
 
 function MyApp({ Component, pageProps }) {
+
+  const [signerAdress, setSignerAddress] = useState("")
+  const [connected, setConnected] = useState(false)
+
+  const connectWallet = async () => {
+    const providerOptions = {
+      /* See Provider Options Section */
+    }
+
+    const web3Modal = new Web3Modal({
+      network: "mainnet", // optional
+      cacheProvider: true, // optional
+      providerOptions // required
+    })
+
+    const provider = await web3Modal.connect()
+    const web3 = new Web3(provider)
+    provider.on("accountsChanged", (accounts) => {
+      if (accounts.length === 0) {
+        setConnected(false)
+      } else {
+        setConnected(true)
+        setSigner(accounts[0])
+      }
+    });
+  }
+
+  const setSigner = (address) => {
+    console.log(address, "aaaa")
+    setSignerAddress(address)
+  }
+
+  useEffect(() => {
+    ethereum.on('accountsChanged', function (accounts) {
+      if (accounts.length !== 0) {
+        setSigner(accounts[0])
+        setConnected(true)
+      } else {
+        setConnected(false)
+      }
+    });
+    if (ethereum.selectedAddress !== null) {
+      setConnected(true)
+      setSignerAddress(ethereum.selectedAddress)
+    }
+  }, [])
+
   return (
     <div className="full-page">
-      <SideMenu />
-      <Component {...pageProps} />
+      <SideMenu
+        connectWallet={connectWallet}
+        connected={connected}
+        address={signerAdress}
+      />
+      <Component {...pageProps}
+      />
     </div>
   )
 }
